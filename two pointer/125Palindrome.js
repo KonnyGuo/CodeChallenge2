668258b1a706fbc786b8f6ca408b3de635002373
KonnyGuo <guokonny@gmail.com>
2024-02-23 19:57:49 -0500

    first

--- two pointer/125Palindrome.js
+++ two pointer/125Palindrome.js
@@ -35,36 +35,3 @@ const reverse = (s) =>
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function (s) {
-  if (s.length <= 1) return true;
-
-  let [left, right] = [0, s.length - 1];
-  let leftChar, rightChar;
-  while (left < right) {
-    leftChar = s[left];
-    rightChar = s[right];
-
-    // skip char if non-alphanumeric
-    if (!/[a-zA-Z0-9]/.test(leftChar)) {
-      left++;
-    } else if (!/[a-zA-Z0-9]/.test(rightChar)) {
-      right--;
-    } else {
-      // compare letters
-      if (leftChar.toLowerCase() != rightChar.toLowerCase()) {
-        return false;
-      }
-      left++;
-      right--;
-    }
-  }
-  return true;
-};
-
-/**
- * 2 Pointer | Midde Convergence | No RegEx | No Copying
- * Time O(N) | Space O(1)
- * https://leetcode.com/problems/valid-palindrome/
- * @param {string} s
- * @return {boolean}
- */
