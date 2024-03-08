ba288b77ee262b9e5b990595ad019ed0caacc37b
KonnyGuo <guokonny@gmail.com>
2024-03-07 22:05:02 -0500

    leet

--- two pointer/3sum.js
+++ two pointer/3sum.js
@@ -2,14 +2,3 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function (nums) {
-  const res = [];
-  nums.sort((a, b) => a - b);
-
-  for (let i = 0; i < nums.length; i++) {
-    const a = nums[i];
-    if (a > 0) break;
-    if (i > 0 && a === nums[i - 1]) continue;
-
-
-};
