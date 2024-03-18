65275b1841ed871f79bc0372b400f32860bf0963
KonnyGuo <guokonny@gmail.com>
2024-03-17 21:31:21 -0400

    leet

--- two pointer/3sum.js
+++ two pointer/3sum.js
@@ -2,3 +2,4 @@
 // technique where theres a left and right pointer starting from beginning and end of array
 // no tables need for these problem
 // O(n) more efficent
+// study space and time complexity more
