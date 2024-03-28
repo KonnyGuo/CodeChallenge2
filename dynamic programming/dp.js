20dae8d83c64912340791ba849c88e4a3a1b7d49
KonnyGuo <guokonny@gmail.com>
2024-03-27 22:29:57 -0400

    leet

--- dynamic programming/dp.js
+++ dynamic programming/dp.js
@@ -1,4 +1,4 @@
-// identify base case
+//  identify base case
 // draw table
 // consider edge cases?
 // consider patterns
@@ -9,3 +9,4 @@
 // fixed schedule
 // prep question
 // work through data cleaning etc
+// start applications again
