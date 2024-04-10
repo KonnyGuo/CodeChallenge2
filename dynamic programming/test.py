e3117defc84c681167200f503156990febdd1480
KonnyGuo <guokonny@gmail.com>
2024-04-09 20:57:40 -0400

    leet

--- dynamic programming/test.py
+++
@@ -1,6 +0,0 @@
-print("py file")
-# excel to csv file and edit data using panda and numoy
-# prep for app
-# finished research convert csv and start
-# understand spreadsheet vs sql also finished csv file and cleanup
-# finsihed py 
