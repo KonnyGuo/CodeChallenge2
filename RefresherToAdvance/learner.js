35be536acbabf4e6b9db4fb9003b57bdeede3fa5
KonnyGuo <guokonny@gmail.com>
2024-04-29 23:32:35 -0400

    edit

--- RefresherToAdvance/learner.js
+++ RefresherToAdvance/learner.js
@@ -1,2 +1,3 @@
 // calculation with dollars best done in cents then divide 100 to convert to dollars becacause of precision and javascripts issue with floats
 // anki methods and overall concepts
+// banki
