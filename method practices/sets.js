4b1c6605434caae6bfc582966f7543593e585014
KonnyGuo <guokonny@gmail.com>
2024-05-17 22:17:59 -0400

    review

--- method practices/sets.js
+++ method practices/sets.js
@@ -1,7 +1,25 @@
-let newSet = new Set();
+let mySet = new Set();
 
-newSet.add(1);
-newSet.add(2);
-newSet.add(3);
+mySet.add(1);
+mySet.add(2);
+mySet.add(3);
+mySet.add(4);
 
-console.log(newSet);
+console.log(mySet);
+console.log(mySet.size);
+
+let bool = mySet.has(2);
+if (bool) {
+  console.log("set contains 2");
+} else {
+  console.log("set does not contain 2");
+}
+
+for (let value of mySet) {
+  console.log(`in for`, value);
+}
+
+mySet.forEach((value) => console.log(`in forEach ${value}`));
+
+mySet.delete(2);
+console.log("deleted 2", mySet);
