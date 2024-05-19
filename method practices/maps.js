e10695454fe5b2d7d9a90f932a1bae09d6666773
KonnyGuo <guokonny@gmail.com>
2024-05-18 22:25:03 -0400

    review

--- method practices/maps.js
+++ method practices/maps.js
@@ -5,10 +5,6 @@ myMap.set("b", 2);
 myMap.set("c", 3);
 myMap.set("d", 4);
 
-console.log(myMap.get("b"));
-console.log(myMap.size);
-console.log(myMap.has("b"));
-
-for (let [key, value] of myMap) {
-  console.log(key, value);
-}
+console.log(myMap.keys());
+let mySet = new Set(myMap.keys());
+console.log(mySet);
