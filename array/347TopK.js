fa166d29a79bef40c3ae7aadca849a1dd6321850
KonnyGuo <guokonny@gmail.com>
2024-05-21 00:24:10 -0400

    leet

--- array/347TopK.js
+++ array/347TopK.js
@@ -97,3 +97,40 @@ var topKFrequent = function (nums, k) {
 
   return output; // Return the array of top k frequent elements
 };
+
+// Time O(N) | Space O(k)
+var topKFrequent = function (nums, k) {
+  const mp = new Map(); // Map to store frequency of each element
+  const arr = new Array(nums.length + 1).fill(0); // Array to store elements grouped by frequency
+  const ans = []; // Array to store the final answer
+
+  // Count frequencies of each element in nums
+  nums.forEach((el) => {
+    const val = mp.get(el) || 0; // Get current frequency or 0 if not present
+    mp.set(el, val + 1); // Increment the frequency
+  });
+
+  // Group elements by their frequencies
+  for (let [key, freq] of mp) {
+    const prev = arr[freq] || []; // Get current array of elements with this frequency or empty array
+    prev.push(key); // Add current element to this array
+    arr[freq] = prev; // Update the array at the index of the frequency
+  }
+
+  // Reverse the array to start with the highest frequencies
+  arr.reverse();
+
+  // Extract the top K frequent elements
+  for (let el of arr) {
+    if (k < 1) break; // Stop if we have added k elements
+    if (el) {
+      for (let el2 of el) {
+        ans.push(el2); // Add the element to the answer
+        k--; // Decrement k
+        if (k === 0) break; // Stop if we have added k elements
+      }
+    }
+  }
+
+  return ans; // Return the final answer
+};
--- array/functionTest.js
+++ array/functionTest.js
@@ -1,10 +1,32 @@
-let nums = [
-  ["Bob", 95],
-  ["David", 95],
-  ["Alice", 85],
-  ["Charlie", 80],
-  ["Eve", 70],
-];
+// let nums = [
+//   ["Bob", 95],
+//   ["David", 95],
+//   ["Alice", 85],
+//   ["Charlie", 80],
+//   ["Eve", 70],
+// ];
 
-nums.sort((a, b) => a[0].localeCompare(b[0]));
-console.log(nums);
+// nums.sort((a, b) => a[0].localeCompare(b[0]));
+// console.log(nums);
+
+const person = {
+  1: "Alice",
+  2: 25,
+};
+
+list = [1, 2, 3, 4];
+
+let result = Object.keys(person).map((p) => {
+  return [Number(p), person[p]];
+});
+
+// for (let i = 0; i < result.length; i++) {
+//   console.log(result[i]);
+// }
+// console.log(...result);
+// console.log(...list);
+
+let newlist = [...list];
+for (let i = 0; i < newlist.length; i++) {
+  console.log(newlist[i]);
+}
