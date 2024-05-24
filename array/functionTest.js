37e5b32b88098b8194a5e221f09804d389723369
KonnyGuo <guokonny@gmail.com>
2024-05-24 10:54:51 -0400

    leet

--- array/functionTest.js
+++ array/functionTest.js
@@ -1,21 +1,24 @@
-function productExceptSelf(nums) {
-  const result = [];
-  let prefix = 1;
-  let postfix = 1;
+// function productExceptSelf(nums) {
+//   const result = [];
+//   let prefix = 1;
+//   let postfix = 1;
 
-  for (let i = 0; i < nums.length; i++) {
-    result[i] = prefix;
-    prefix = prefix * nums[i];
-    console.log("Iteration", i, "prefix", result[i]);
-  }
-  for (let i = nums.length - 2; i >= 0; i--) {
-    postfix = postfix * nums[i + 1];
-    result[i] = result[i] * postfix;
-    console.log("Iteration", i, "postfix", result[i]);
-  }
+//   for (let i = 0; i < nums.length; i++) {
+//     result[i] = prefix;
+//     prefix = prefix * nums[i];
+//     console.log("Iteration", i, "prefix", result[i]);
+//   }
+//   for (let i = nums.length - 2; i >= 0; i--) {
+//     postfix = postfix * nums[i + 1];
+//     result[i] = result[i] * postfix;
+//     console.log("Iteration", i, "postfix", result[i]);
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
-let nums = [1, 2, 3, 4];
-productExceptSelf(nums);
+// let nums = [1, 2, 3, 4];
+// productExceptSelf(nums);
+
+let testList = [1, 2];
+console.log(testList[2]);
