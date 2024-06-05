ff7271cb929241d7c55f985e5b85360ecf3e502e
KonnyGuo <guokonny@gmail.com>
2024-06-04 22:55:54 -0400

    leet

--- RefresherToAdvance/learner.js
+++ RefresherToAdvance/learner.js
@@ -1,13 +1,11 @@
 // anki methods and overall concepts
 // banki
-// functional + data structures
-// r
+
 // sql + python pandas
 // version control
 // finish assessment (almost completed) / dashboards (5 days)
 // work csv file, py collab
 // tableau refresher and import data sets
-// tdesk work and cert
 // test git date
 // git date formate test complete
 // git version control refresher/practice/using branches/forking v cloning
