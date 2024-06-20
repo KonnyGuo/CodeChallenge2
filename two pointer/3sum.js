f7d068993579244a27063d5f4158af15eeaa18fa
KonnyGuo <guokonny@gmail.com>
2024-06-19 21:52:56 -0400

    leet

--- two pointer/3sum.js
+++ two pointer/3sum.js
@@ -1,4 +1,5 @@
 /**
+ * Time O(N^2) | Space O(N)
  * @param {number[]} nums
  * @return {number[][]}
  */
@@ -24,6 +25,47 @@ var threeSum = function (nums) {
         l++;
         r--;
         while (nums[l] === nums[l - 1] && l < r) {
+          l++;
+        }
+      }
+    }
+  }
+  return res;
+};
+
+/**
+ * Time O(N^2) | Space O(N)
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  const res = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    const a = nums[i];
+    if (a > 0) break;
+
+    // Skip duplicates for 'a'
+    if (i > 0 && a === nums[i - 1]) {
+      continue;
+    }
+
+    let l = i + 1;
+    let r = nums.length - 1;
+    while (l < r) {
+      const threeSum = a + nums[l] + nums[r];
+      if (threeSum > 0) {
+        r--;
+      } else if (threeSum < 0) {
+        l++;
+      } else {
+        res.push([a, nums[l], nums[r]]);
+        l++;
+        r--;
+
+        // Skip duplicates for 'l'
+        while (l < r && nums[l] === nums[l - 1]) {
           l++;
         }
       }
