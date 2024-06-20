f9977b6078ff2af6ff42f3ef5c7b4607f5315e09
KonnyGuo <guokonny@gmail.com>
2024-06-19 21:44:58 -0400

    leet

--- two pointer/167TwoSum2.js
+++ two pointer/167TwoSum2.js
@@ -25,3 +25,25 @@ var twoSum = function (numbers, target) {
 
   return [-1, -1];
 };
+
+var twoSum = function (numbers, target) {
+  let [left, right] = [0, numbers.length - 1];
+
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
+
+    // adding 1 because problem
+    if (sum === target) {
+      return [left + 1, right + 1];
+    }
+
+    // if sum less than target (means left side too small)
+    if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return [-1, -1];
+};
