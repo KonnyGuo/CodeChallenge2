0651c7aff04a2317d565af70447e65ac097f4d8c
KonnyGuo <guokonny@gmail.com>
2024-06-19 21:51:17 -0400

    leet

--- two pointer/3sum.js
+++ two pointer/3sum.js
@@ -1,5 +1,33 @@
-// to be continued
-// technique where theres a left and right pointer starting from beginning and end of array
-// no tables need for these problem
-// O(n) more efficent
-// study space and time complexity more
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  const res = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    const a = nums[i];
+    if (a > 0) break;
+    if (i > 0 && a === nums[i - 1]) continue;
+
+    let l = i + 1;
+    let r = nums.length - 1;
+    while (l < r) {
+      const threeSum = a + nums[l] + nums[r];
+      if (threeSum > 0) {
+        r--;
+      } else if (threeSum < 0) {
+        l++;
+      } else {
+        res.push([a, nums[l], nums[r]]);
+        l++;
+        r--;
+        while (nums[l] === nums[l - 1] && l < r) {
+          l++;
+        }
+      }
+    }
+  }
+  return res;
+};
