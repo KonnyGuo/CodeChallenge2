ee68f0177d343bc73a84853b459ca8bf3c5e55fe
KonnyGuo <guokonny@gmail.com>
2024-06-23 20:37:07 -0400

    leet

--- sliding window/121buyAndSellStock.js
+++ sliding window/121buyAndSellStock.js
@@ -42,3 +42,26 @@ var maxProfit = function (prices) {
   }
   return value;
 };
+
+/**
+ * Time O(N) | Space O(1)
+ * @param {number} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let left = 0; // Initialize the left pointer
+  let right = 1; // Initialize the right pointer
+  let maxProfit = 0; // Initialize the max profit
+
+  while (right < prices.length) {
+    if (prices[right] <= prices[left]) {
+      left = right; // Move the left pointer to the right
+    } else {
+      const profit = prices[right] - prices[left]; // Calculate the profit
+      maxProfit = Math.max(maxProfit, profit); // Update the max profit
+    }
+    right++; // Move the right pointer to the next day
+  }
+
+  return maxProfit; // Return the maximum profit found
+};
