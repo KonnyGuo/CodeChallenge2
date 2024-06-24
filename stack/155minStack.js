216792c7ef18c5e36efc3d0a7eded0d538135d52
KonnyGuo <guokonny@gmail.com>
2024-06-24 18:00:36 -0400

    leet

--- stack/155minStack.js
+++ stack/155minStack.js
@@ -97,3 +97,76 @@ class Node {
     this.next = next;
   }
 }
+
+/**
+ * https://leetcode.com/problems/min-stack
+ * Time O(1) | Space O(N)
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+class MinStack {
+  /**
+   * Initializes the MinStack.
+   */
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  /**
+   * Pushes an element onto the stack.
+   * @param {number} val - The value to be pushed onto the stack.
+   * @returns {void}
+   */
+  push(val) {
+    this.stack.push(val); // Space O(N)
+
+    const isMinEmpty = this.minStack.length === 0;
+    const hasNewMin =
+      isMinEmpty || val <= this.minStack[this.minStack.length - 1];
+    if (hasNewMin) {
+      this.minStack.push(val); // Space O(N)
+    }
+  }
+
+  /**
+   * Removes the element on top of the stack.
+   * @returns {void}
+   */
+  pop() {
+    if (this.stack.length === 0) {
+      return;
+    }
+    const top = this.stack.pop(); // Time O(1)
+
+    if (top === this.minStack[this.minStack.length - 1]) {
+      this.minStack.pop(); // Time O(1)
+    }
+  }
+
+  /**
+   * Get the top element of the stack.
+   * @returns {number|null} The top element of the stack, or null if the stack is empty.
+   */
+  top() {
+    if (this.stack.length === 0) {
+      return null;
+    }
+    return this.stack[this.stack.length - 1]; // Time O(1)
+  }
+
+  /**
+   * Retrieve the minimum element in the stack.
+   * @returns {number|null} The minimum element in the stack, or null if the stack is empty.
+   */
+  getMin() {
+    if (this.minStack.length === 0) {
+      return null;
+    }
+    return this.minStack[this.minStack.length - 1]; // Time O(1)
+  }
+}
