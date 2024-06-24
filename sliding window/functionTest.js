15695cee858671e3c1c7dc65740a5e3581bf73eb
KonnyGuo <guokonny@gmail.com>
2024-06-24 14:29:58 -0400

    leet

--- sliding window/functionTest.js
+++ sliding window/functionTest.js
@@ -1,2 +1,8 @@
-let string1 = "abc";
-console.log(string1.charAt(1));
+// let string1 = "abc";
+// console.log(string1.charAt(1));
+
+let testObj = { a: 0 };
+let string = "a";
+if (string in testObj) {
+  console.log("yes");
+}
