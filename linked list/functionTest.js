7e305d996de62c6e81aef1c6c41028f418c7555a
KonnyGuo <guokonny@gmail.com>
2024-06-29 20:18:34 -0400

    leet

---
+++ linked list/functionTest.js
@@ -0,0 +1,25 @@
+// Define a node class
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+// Function to print nodes of the linked list
+function printList(head) {
+  let currentNode = head; // Start with the head node
+  while (currentNode !== null) {
+    // Continue until the end of the list
+    console.log(currentNode); // Print the value of the current node
+    currentNode = currentNode.next; // Move to the next node
+  }
+}
+
+// Create nodes
+let node3 = new ListNode(3, null);
+let node2 = new ListNode(2, node3);
+let node1 = new ListNode(1, node2);
+
+// Head of the list
+let head = node1;
+
+console.log(head);
