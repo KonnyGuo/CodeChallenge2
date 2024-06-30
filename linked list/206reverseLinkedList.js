2341d59b7150a7db7dae2ef545e61f4e7be27ee3
KonnyGuo <guokonny@gmail.com>
2024-06-29 20:28:47 -0400

    leet

--- linked list/206reverseLinkedList.js
+++ linked list/206reverseLinkedList.js
@@ -22,6 +22,30 @@ const dfs = (curr) => {
 
 /**
  * https://leetcode.com/problems/reverse-linked-list/
+ * Time O(N) | Space O(N)
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  const isBaseCase = head === null || head.next === null;
+  if (isBaseCase) return head;
+
+  const dfs = (curr) => {
+    if (curr.next === null) return curr;
+
+    const prev = dfs(curr.next);
+
+    curr.next.next = curr;
+    curr.next = null;
+
+    return prev;
+  };
+
+  return dfs(head);
+};
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
  * Time O(N) | Space O(1)
  * @param {ListNode} head
  * @return {ListNode}
