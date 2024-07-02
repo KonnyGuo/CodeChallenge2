4896641c91fda640f1f59cb80294c5eb1ad5114f
KonnyGuo <guokonny@gmail.com>
2024-07-02 19:25:36 -0400

    leet

--- backtracking/functionTest.js
+++ backtracking/functionTest.js
@@ -1,5 +1,25 @@
-let num = 5;
-let set = [[]];
-let next = [...set, num];
+// var subsets = function (nums) {
+//   nums.sort((a, b) => a - b);
 
-console.log(next);
+//   let subsets = [[]];
+
+//   for (const num of nums) {
+//     console.log("num", num);
+//     const levels = subsets.length;
+
+//     for (let level = 0; level < levels; level++) {
+//       const nextLevel = [...subsets[level], num];
+//       console.log("nextLevel", nextLevel);
+//       subsets.push(nextLevel);
+//       console.log("subsets", subsets);
+//     }
+//   }
+
+//   return subsets;
+// };
+
+// subsets([1, 2, 3]);
+
+let test = [[]];
+console.log([...test[0], 1]);
+console.log(test[0]);
