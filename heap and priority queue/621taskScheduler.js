3f217a174e5b4d4ec3c281d28e54a1aecf1bb4e8
KonnyGuo <guokonny@gmail.com>
2024-07-02 00:32:40 -0400

    leet

--- heap and priority queue/621taskScheduler.js
+++ heap and priority queue/621taskScheduler.js
@@ -153,3 +153,21 @@ var leastInterval = function (tasks, n) {
   // Return the maximum of the calculated interval and the total number of tasks
   return Math.max(tasks.length, interval);
 };
+
+// Why we track maxFrequency and mostFrequentTask:
+
+// maxFrequency: This tells us how many times we need to schedule the most frequent task(s). It determines the "skeleton" of our schedule.
+// mostFrequentTask: This counts how many tasks share the maximum frequency. It's crucial for determining if we need extra slots at the end of our schedule.
+
+// How the interval math works:
+// The formula is: ((maxFrequency - 1) * (n + 1)) + mostFrequentTask
+// Let's break this down:
+// a) (maxFrequency - 1): This represents the number of "full" cycles we need. We subtract 1 because the last occurrence doesn't need idle time after it.
+// b) (n + 1): This is the length of each cycle. It includes the task itself (1) plus the idle time (n).
+// c) ((maxFrequency - 1) * (n + 1)): This gives us the total length of all full cycles.
+// d) + mostFrequentTask: This adds slots for the final occurrence of each of the most frequent tasks.
+// Why this works:
+
+// The formula creates a "skeleton" schedule based on the most frequent task(s).
+// Other less frequent tasks can be slotted into the idle times without extending the overall length.
+// If there are multiple tasks with the max frequency, we need extra slots at the end to accommodate them all.
