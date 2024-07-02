668bdd2864db36e04026c0536eadb9f91b4590e2
KonnyGuo <guokonny@gmail.com>
2024-07-01 22:09:00 -0400

    leet

--- heap and priority queue/215kthLargestInArray.js
+++ heap and priority queue/215kthLargestInArray.js
@@ -2,12 +2,44 @@
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
+ * Time O(n * log(k)) | Space O(k)
  */
-// time O(n * log(k)) | space O(k)
 var findKthLargest = function (nums, k) {
   nums.sort((a, b) => {
     return a - b;
   });
 
   return nums[nums.length - k];
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+  // find the largest element
+  let largest = -Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > largest) largest = nums[i];
+  }
+
+  // construct a hash map of difference between nums[i] and largest
+  const hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = largest - nums[i];
+    hash[diff] = (hash[diff] || 0) + 1;
+  }
+
+  // find kth largest number
+  let count = 0;
+  let diff = 0;
+  while (count < k) {
+    count += hash[diff] || 0;
+    diff++;
+  }
+
+  return largest - diff + 1;
 };
--- heap and priority queue/973kClosestToOrigin.js
+++ heap and priority queue/973kClosestToOrigin.js
@@ -1,6 +1,10 @@
-// time: O(N log(N))
-// space: O(1)
-// https://leetcode.com/problems/k-closest-points-to-origin/description/
+/**
+ * // https://leetcode.com/problems/k-closest-points-to-origin/description/
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ * time O(N * log(N)) | space O(N)
+ */
 var kClosest = function (points, K) {
   points.sort(
     (a, b) => a[0] * a[0] + a[1] * a[1] - (b[0] * b[0] + b[1] * b[1])
