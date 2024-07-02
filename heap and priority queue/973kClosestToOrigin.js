c92164facbdf2c2c19b03c7263d5569f7c380028
KonnyGuo <guokonny@gmail.com>
2024-07-01 21:58:59 -0400

    leet

---
+++ heap and priority queue/973kClosestToOrigin.js
@@ -0,0 +1,9 @@
+// time: O(N log(N))
+// space: O(1)
+var kClosest = function (points, K) {
+  points.sort(
+    (a, b) => a[0] * a[0] + a[1] * a[1] - (b[0] * b[0] + b[1] * b[1])
+  );
+
+  return points.slice(0, K);
+};
