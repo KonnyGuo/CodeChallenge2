7006d064b3203630a746fdf5df5295f90c93c9ed
KonnyGuo <guokonny@gmail.com>
2024-07-02 22:27:08 -0400

    leet

--- graphs/133cloneGraph.js
+++ graphs/133cloneGraph.js
@@ -71,3 +71,38 @@ const cloneNeighbors = (node, seen, queue) => {
     parentClone.neighbors.push(neighborClone); /* Space O(V + E) */
   }
 };
+
+/**
+ * Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+/**
+ * Time O(N + E) | Space(O(N))
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function (node) {
+  if (!node) return null;
+
+  const visited = new Map();
+
+  function dfs(originalNode) {
+    if (visited.has(originalNode)) {
+      return visited.get(originalNode);
+    }
+
+    const clonedNode = new Node(originalNode.val);
+    visited.set(originalNode, clonedNode);
+
+    for (let neighbor of originalNode.neighbors) {
+      clonedNode.neighbors.push(dfs(neighbor));
+    }
+
+    return clonedNode;
+  }
+
+  return dfs(node);
+};
