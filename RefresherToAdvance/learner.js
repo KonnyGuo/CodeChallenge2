4008ccd25b101e7e125c734da1d03273b24d15c6
KonnyGuo <guokonny@gmail.com>
2024-07-15 23:28:10 -0400

    leet

--- RefresherToAdvance/learner.js
+++ RefresherToAdvance/learner.js
@@ -2,10 +2,7 @@
 // banki
 
 // sql + python pandas
-// version control
-// finish assessment (almost completed) / dashboards (5 days)
 // work csv file, py collab
-// tableau refresher and import data sets
 // git version control refresher/practice/using branches/forking v cloning
 // vue refresher + aws/cloud
 // utilize tableau dashboard to create more spacing
