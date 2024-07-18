008b263ab1622ad41630c090bed4b460cc04f9ec
KonnyGuo <guokonny@gmail.com>
2024-07-17 22:34:19 -0400

    leet

--- array/58lengthOfLastWord.js
+++ array/58lengthOfLastWord.js
@@ -30,3 +30,26 @@ var lengthOfLastWord = function (s) {
   }
   return lastWordLen;
 };
+
+/**
+ * time O(n) | space O(1)
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  let length = 0;
+  let i = s.length - 1;
+
+  // Skip trailing spaces
+  while (i >= 0 && s[i] === " ") {
+    i--;
+  }
+
+  // Count characters of the last word
+  while (i >= 0 && s[i] !== " ") {
+    length++;
+    i--;
+  }
+
+  return length;
+};
