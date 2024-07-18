38ca6f32f5f3b9625a8b1fd89dcc8794aea261d2
KonnyGuo <guokonny@gmail.com>
2024-07-17 22:26:18 -0400

    leet

--- array/392isSubseq.js
+++ array/392isSubseq.js
@@ -35,3 +35,23 @@ var isSubsequence = function (s, t) {
   }
   return true;
 };
+
+/**
+ * time O(n) | space O(1)
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  let sPointer = 0;
+  let tPointer = 0;
+
+  while (sPointer < s.length && tPointer < t.length) {
+    if (s[sPointer] === t[tPointer]) {
+      sPointer++;
+    }
+    tPointer++;
+  }
+
+  return sPointer === s.length;
+};
