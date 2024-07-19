1a2ad9c9a2f6f1aa5974ad539add6537c0efba32
KonnyGuo <guokonny@gmail.com>
2024-07-18 22:03:35 -0400

    leet

--- array/27removeElement.js
+++ array/27removeElement.js
@@ -50,3 +50,22 @@ var removeElement = function (nums, val) {
 
   return nums.length;
 };
+
+/**
+ * time O(N) | space O(1)
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let k = 0; // pointer for the position to place non-val elements
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
