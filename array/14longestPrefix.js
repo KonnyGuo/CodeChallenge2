e66ec84513fdb36de8da66ff89ba53d44e9337bc
KonnyGuo <guokonny@gmail.com>
2024-07-18 21:27:21 -0400

    leet

--- array/14longestPrefix.js
+++ array/14longestPrefix.js
@@ -16,3 +16,23 @@ var longestCommonPrefix = function (strs) {
 
   return pre;
 };
+
+/**
+ * time O(N) | space O(1)
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return "";
+
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === "") return "";
+    }
+  }
+
+  return prefix;
+};
