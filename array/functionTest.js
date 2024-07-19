d066f4e3b702218dd43baa9df4610c6ff268eb1b
KonnyGuo <guokonny@gmail.com>
2024-07-18 21:49:32 -0400

    leet

--- array/functionTest.js
+++ array/functionTest.js
@@ -1,9 +1,13 @@
-let obj = {};
+// let obj = {};
 
-if (obj.hasOwnProperty(1)) {
-  obj[1] = obj[1] + 1;
-} else {
-  obj[1] = 1;
-}
+// if (obj.hasOwnProperty(1)) {
+//   obj[1] = obj[1] + 1;
+// } else {
+//   obj[1] = 1;
+// }
 
-console.log(Object.entries(obj));
+// console.log(Object.entries(obj));
+
+let str1 = "flower";
+let str2 = "flow";
+console.log(str2.indexOf(str1));
