ffe64a291aa2c1403a7ea5894fea0b41054a97ef
KonnyGuo <guokonny@gmail.com>
2024-07-20 23:22:51 -0400

    leet

--- arrayAndHashing/functionTest.js
+++ arrayAndHashing/functionTest.js
@@ -1,12 +1,12 @@
-// let obj = {};
+let obj = {};
 
-// if (obj.hasOwnProperty(1)) {
-//   obj[1] = obj[1] + 1;
-// } else {
-//   obj[1] = 1;
-// }
+if (obj.hasOwnProperty(1)) {
+  obj[1] = obj[1] + 1;
+} else {
+  obj[1] = 1;
+}
 
-// console.log(Object.entries(obj));
+console.log(Object.entries(obj));
 
 // if str1 larger than str2 then it will be -1 aka no found
 let str1 = "flower";
