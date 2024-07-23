3a404fcbabcb06def27407a4721be6d3c81b3d0b
KonnyGuo <guokonny@gmail.com>
2024-07-22 20:23:41 -0400

    leet

--- arrayAndHashing/605canReplaceFlower.js
+++ arrayAndHashing/605canReplaceFlower.js
@@ -21,3 +21,30 @@ var canPlaceFlowers = function (fb, n) {
 
   return false;
 };
+
+/**
+ * Loop Solution
+ * Time O(N) | Space O(1)
+ * https://leetcode.com/problems/can-place-flowers
+ * @param {number[]} fb
+ * @param {number} n
+ * @return {boolean}
+ */
+function canPlaceFlowers(flowerbed, n) {
+  let count = 0;
+  const size = flowerbed.length;
+
+  for (let i = 0; i < size; i++) {
+    if (flowerbed[i] === 0) {
+      const emptyLeft = i === 0 || flowerbed[i - 1] === 0;
+      const emptyRight = i === size - 1 || flowerbed[i + 1] === 0;
+
+      if (emptyLeft && emptyRight) {
+        flowerbed[i] = 1;
+        count++;
+      }
+    }
+  }
+
+  return count >= n;
+}
