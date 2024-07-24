eb738a0d5cc511f25d1599f2bf1471b845b38c89
KonnyGuo <guokonny@gmail.com>
2024-07-23 21:51:43 -0400

    leet

--- arrayAndHashing/169majorityElement.js
+++ arrayAndHashing/169majorityElement.js
@@ -65,3 +65,22 @@ function majorityElement(nums) {
 
   return candidate;
 }
+
+function majorityElement(nums) {
+  let candidate = 0;
+  let count = 0;
+
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+
+    if (num === candidate) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return candidate;
+}
