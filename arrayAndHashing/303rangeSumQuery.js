f84afad6dccb2ad9efc875ef9ac7d76edd970ad6
KonnyGuo <guokonny@gmail.com>
2024-07-25 21:21:03 -0400

    leet

--- arrayAndHashing/303rangeSumQuery.js
+++ arrayAndHashing/303rangeSumQuery.js
@@ -37,3 +37,18 @@ var NumArray = function (nums) {
     this.prefixSum[i + 1] = this.prefixSum[i] + nums[i];
   }
 };
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+  return this.prefixSum[right + 1] - this.prefixSum[left];
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
