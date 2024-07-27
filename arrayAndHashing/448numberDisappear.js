0a28f9aeec4c762067757250a7c88e2a14ad6eaf
KonnyGuo <guokonny@gmail.com>
2024-07-26 22:13:43 -0400

    leet

--- arrayAndHashing/448numberDisappear.js
+++ arrayAndHashing/448numberDisappear.js
@@ -16,3 +16,24 @@ var findDisappearedNumbers = function (nums) {
 
   return Array.from(numberSet);
 };
+
+function findDisappearedNumbers(nums) {
+  const result = [];
+
+  // Mark numbers that are present
+  for (let i = 0; i < nums.length; i++) {
+    let index = Math.abs(nums[i]) - 1;
+    if (nums[index] > 0) {
+      nums[index] = -nums[index];
+    }
+  }
+
+  // Find unmarked numbers
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result.push(i + 1);
+    }
+  }
+
+  return result;
+}
