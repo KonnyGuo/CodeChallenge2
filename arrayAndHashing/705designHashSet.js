231278ae0e449957d93f7a8b11b3cf3ab0fd13d1
KonnyGuo <guokonny@gmail.com>
2024-07-27 22:37:28 -0400

    leet

--- arrayAndHashing/705designHashSet.js
+++ arrayAndHashing/705designHashSet.js
@@ -39,3 +39,37 @@ class MyHashSet {
  * obj.remove(key)
  * var param_3 = obj.contains(key)
  */
+
+class MyHashSet {
+  constructor() {
+    this.size = 1000; // Size of the bucket array
+    this.buckets = new Array(this.size).fill().map(() => []);
+  }
+
+  hash(key) {
+    return key % this.size;
+  }
+
+  add(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    if (!bucket.includes(key)) {
+      bucket.push(key);
+    }
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    const keyIndex = bucket.indexOf(key);
+    if (keyIndex !== -1) {
+      bucket.splice(keyIndex, 1);
+    }
+  }
+
+  contains(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    return bucket.includes(key);
+  }
+}
