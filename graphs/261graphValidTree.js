5b9861a386d36858d1c260f1120fb9d402d7b0bb
KonnyGuo <guokonny@gmail.com>
2024-08-04 20:20:32 -0400

    leet

--- graphs/261graphValidTree.js
+++ graphs/261graphValidTree.js
@@ -40,3 +40,61 @@ const dfs = (node, graph, visited) => {
     dfs(neighbor, graph, visited);
   }
 };
+
+/**
+ * https://leetcode.com/problems/graph-valid-tree/
+ * Time O(E * a(N)) | Space O(V)
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {boolean}
+ */
+var validTree = function (n, edges) {
+  const isEqual = edges.length === n - 1;
+  if (!isEqual) return false;
+
+  const { graph, visited, queue } = buildGraph(n, edges);
+
+  bfs(graph, visited, queue);
+
+  return visited.size === n;
+};
+
+var initGraph = (n) => ({
+  graph: new Array(n).fill().map(() => []),
+  visited: new Set(),
+  queue: new Queue(),
+  root: 0,
+});
+
+var buildGraph = (n, edges) => {
+  const { graph, visited, queue, root } = initGraph(n);
+
+  for (const [src, dst] of edges) {
+    graph[src].push(dst);
+    graph[dst].push(src);
+  }
+
+  queue.enqueue(root);
+  visited.add(root);
+
+  return { graph, visited, queue };
+};
+
+const bfs = (graph, visited, queue) => {
+  while (!queue.isEmpty()) {
+    for (let i = queue.size() - 1; 0 <= i; i--) {
+      checkNeighbor(graph, visited, queue);
+    }
+  }
+};
+
+const checkNeighbor = (graph, visited, queue) => {
+  const node = queue.dequeue();
+
+  for (const neighbor of graph[node]) {
+    if (visited.has(neighbor)) continue;
+    visited.add(neighbor);
+
+    queue.enqueue(neighbor);
+  }
+};
