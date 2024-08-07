d03da293e71bda8981464b1908ee225209a2fa68
KonnyGuo <guokonny@gmail.com>
2024-08-06 21:47:40 -0400

    leet

---
+++ advanced graphs/743networkDelay.js
@@ -0,0 +1,10 @@
+/**
+ * Graph - BFS
+ * Queue - Space (WIDTH)
+ * Array - Greedy
+ * https://leetcode.com/problems/network-delay-time/
+ * @param {number[][]} times
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
