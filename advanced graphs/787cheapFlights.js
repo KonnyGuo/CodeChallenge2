f2bd2909f569af0d701e748864e04526d2a78a32
KonnyGuo <guokonny@gmail.com>
2024-08-09 21:46:04 -0400

    leet

--- advanced graphs/787cheapFlights.js
+++ advanced graphs/787cheapFlights.js
@@ -18,3 +18,36 @@ var initGraph = (n) => ({
   seen: new Map(),
   minHeap: new MinPriorityQueue(),
 });
+
+var buildGraph = (n, flights, src, dst, K) => {
+  const { graph, seen, minHeap } = initGraph(n);
+
+  for (const [src, dst, cost] of flights) {
+    graph[src].push([dst, cost]);
+  }
+
+  const priority = 0;
+  const node = [priority, src, K + 1];
+
+  minHeap.enqueue(node, priority);
+
+  return { graph, seen, minHeap };
+};
+
+const search = (graph, src, dst, seen, minHeap) => {
+  while (!minHeap.isEmpty()) {
+    const [cost, city, stops] = minHeap.dequeue().element;
+
+    seen.set(city, stops);
+
+    const isTarget = city === dst;
+    if (isTarget) return cost;
+
+    const canSkip = stops <= 0;
+    if (canSkip) continue;
+
+    checkNeighbors(graph, cost, city, stops, seen, minHeap);
+  }
+
+  return -1;
+};
