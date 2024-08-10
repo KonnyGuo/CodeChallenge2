2ad6f5d60b5f5744f31dab151875d2e65533c2ac
KonnyGuo <guokonny@gmail.com>
2024-08-09 21:45:53 -0400

    leet

--- advanced graphs/787cheapFlights.js
+++ advanced graphs/787cheapFlights.js
@@ -6,3 +6,15 @@
  * @param {number} k
  * @return {number}
  */
+
+var findCheapestPrice = function (n, flights, src, dst, K) {
+  const { graph, seen, minHeap } = buildGraph(n, flights, src, dst, K);
+
+  return search(graph, src, dst, seen, minHeap);
+};
+
+var initGraph = (n) => ({
+  graph: new Array(n).fill().map(() => []),
+  seen: new Map(),
+  minHeap: new MinPriorityQueue(),
+});
