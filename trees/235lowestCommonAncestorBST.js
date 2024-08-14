5a0cb5f228d98cd9225425651c9a60ab42917d71
KonnyGuo <guokonny@gmail.com>
2024-08-13 20:55:44 -0400

    leet

--- trees/235lowestCommonAncestorBST.js
+++ trees/235lowestCommonAncestorBST.js
@@ -24,3 +24,22 @@ var lowestCommonAncestor = function (root, p, q) {
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+var lowestCommonAncestor = function (root, p, q) {
+  while (root !== null) {
+    const isGreater = root.val < p.val && root.val < q.val;
+    if (isGreater) {
+      root = root.right;
+      continue;
+    }
+
+    const isLess = p.val < root.val && q.val < root.val;
+    if (isLess) {
+      root = root.left;
+      continue;
+    }
+
+    break;
+  }
+
+  return root;
+};
