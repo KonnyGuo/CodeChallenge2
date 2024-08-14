74d8c4f216aec6ef68f1cd3311c0dd746698d249
KonnyGuo <guokonny@gmail.com>
2024-08-13 20:55:12 -0400

    leet

---
+++ trees/235lowestCommonAncestorBST.js
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * Time O(N) | Space O(H)
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
