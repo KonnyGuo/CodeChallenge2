31c5a149fe8bcc5d6e74cc9264beacc6c7cb0c6d
KonnyGuo <guokonny@gmail.com>
2024-08-15 20:53:38 -0400

    leet

--- trees/199binaryTreeRightSide.js
+++ trees/199binaryTreeRightSide.js
@@ -4,3 +4,28 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+var rightSideView = function (root) {
+  const isBaseCase = root === null;
+  if (isBaseCase) return [];
+
+  return bfs([root]);
+};
+
+const bfs = (queue, rightSide = []) => {
+  while (queue.length) {
+    let prev = null;
+
+    for (let i = queue.length - 1; 0 <= i; i--) {
+      const node = queue.shift();
+
+      prev = node;
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    rightSide.push(prev.val);
+  }
+
+  return rightSide;
+};
