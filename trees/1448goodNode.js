1e5a98c5d2c7b9226e38794163a5ce39e7a38074
KonnyGuo <guokonny@gmail.com>
2024-08-16 22:01:30 -0400

    leet

--- trees/1448goodNode.js
+++ trees/1448goodNode.js
@@ -41,3 +41,21 @@ var goodNodes = function (root) {
 
   return bfs([[root, -Infinity]]);
 };
+
+const bfs = (queue, total = 0) => {
+  while (queue.length) {
+    for (let i = queue.length - 1; 0 <= i; i--) {
+      let [root, max] = queue.shift();
+
+      const isGood = max <= root.val;
+      if (isGood) total++;
+
+      max = Math.max(max, root.val);
+
+      if (root.right) queue.push([root.right, max]);
+      if (root.left) queue.push([root.left, max]);
+    }
+  }
+
+  return total;
+};
