8b756074e656a10f3726085a22ef39773e33160e
KonnyGuo <guokonny@gmail.com>
2024-08-17 21:21:37 -0400

    leet

--- trees/98validateBinaryTree.js
+++ trees/98validateBinaryTree.js
@@ -4,3 +4,20 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+
+var isValidBST = function (root, min = -Infinity, max = Infinity) {
+  const isBaseCase = root === null;
+  if (isBaseCase) return true;
+
+  const isInvalid = root.val <= min || max <= root.val;
+  if (isInvalid) return false;
+
+  return dfs(root, min, max);
+};
+
+const dfs = (root, min, max) => {
+  const left = isValidBST(root.left, min, root.val);
+  const right = isValidBST(root.right, root.val, max);
+
+  return left && right;
+};
