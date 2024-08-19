7894ac80b29e0846f65b3cddf4c773760da0a679
KonnyGuo <guokonny@gmail.com>
2024-08-18 21:38:35 -0400

    leet

--- trees/230kthSmallestElementTree.js
+++ trees/230kthSmallestElementTree.js
@@ -29,3 +29,23 @@ const dfs = (root, k, inOrder) => {
  * @param {number} k
  * @return {number}
  */
+
+var kthSmallest = function (root, k, stack = []) {
+  while (k--) {
+    root = moveLeft(root, stack);
+
+    const isSmallest = k === 0;
+    if (isSmallest) return root.val;
+
+    root = root.right;
+  }
+};
+
+const moveLeft = (root, stack) => {
+  while (root !== null) {
+    stack.push(root);
+    root = root.left;
+  }
+
+  return stack.pop();
+};
