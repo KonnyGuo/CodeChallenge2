f56c0cafc974a48841879f5149b9a772ed65e73a
KonnyGuo <guokonny@gmail.com>
2024-08-18 21:38:24 -0400

    leet

--- trees/230kthSmallestElementTree.js
+++ trees/230kthSmallestElementTree.js
@@ -21,3 +21,11 @@ const dfs = (root, k, inOrder) => {
 
   return inOrder[k - 1];
 };
+
+/**
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+ * Time O(N + K) | Space O(H)
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
